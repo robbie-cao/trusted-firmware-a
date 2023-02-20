89211443dd88d7a74b02654aae1fa53449f8503f
Peter Hoyes <Peter.Hoyes@arm.com>
2023-02-20 12:08:43 +0000

    feat(rdkronos): Introduce Arm RD-Kronos platform
    Create a new platform for the RD-Kronos automotive FVP. This
    platform contains:
     * A GICv4-compatible GIC-700
     * An SCP for CPU power control
     * A secure watchdog
     * 128 MB of SRAM, of which 1 MB is reserved for TF-A
    
    Add FW_CONFIG and TB_FW_CONFIG device trees for a minimal boot.
    
    This platform only supports the BL2_AT_EL3 boot flow.
    
    Upstream-Status: Pending
    Signed-off-by: Peter Hoyes <Peter.Hoyes@arm.com>

---
+++ plat/arm/board/rdkronos/platform.mk
@@ -0,0 +1,63 @@
+# Copyright (c) 2023, ARM Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+RDKRONOS_BASE		=	plat/arm/board/rdkronos
+
+PLAT_INCLUDES		+=	-I${RDKRONOS_BASE}/include/
+
+override ARM_PLAT_MT			:= 1
+override ARM_RECOM_STATE_ID_ENC		:= 1
+override CSS_LOAD_SCP_IMAGES		:= 0
+override CTX_INCLUDE_AARCH32_REGS	:= 0
+override ENABLE_AMU			:= 1
+override NEED_BL1			:= 0
+override NEED_BL2U			:= 0
+override PSCI_EXTENDED_STATE_ID		:= 1
+
+BL2_AT_EL3				:= 1
+CSS_USE_SCMI_SDS_DRIVER			:= 1
+ENABLE_FEAT_FGT				:= 1
+ENABLE_FEAT_ECV				:= 1
+HW_ASSISTED_COHERENCY			:= 1
+NEED_BL32				:= 0
+USE_COHERENT_MEM			:= 0
+
+GIC_ENABLE_V4_EXTN	:= 1
+include drivers/arm/gic/v3/gicv3.mk
+RDKRONOS_GIC_SOURCES	:=	${GICV3_SOURCES}			\
+				plat/common/plat_gicv3.c		\
+				plat/arm/common/arm_gicv3.c
+
+RDKRONOS_CPU_SOURCES	:=	lib/cpus/aarch64/neoverse_poseidon.S
+
+PLAT_BL_COMMON_SOURCES	+=	${RDKRONOS_BASE}/rdkronos_plat.c	\
+				${RDKRONOS_BASE}/include/rdkronos_helpers.S
+
+BL2_SOURCES		+=	${RDKRONOS_BASE}/rdkronos_err.c		\
+				lib/utils/mem_region.c			\
+				plat/arm/common/arm_nor_psci_mem_protect.c
+
+BL31_SOURCES		+=	${RDKRONOS_CPU_SOURCES}			\
+				${RDKRONOS_GIC_SOURCES}			\
+				${RDKRONOS_BASE}/rdkronos_bl31_setup.c	\
+				${RDKRONOS_BASE}/rdkronos_topology.c	\
+				drivers/cfi/v2m/v2m_flash.c		\
+				lib/utils/mem_region.c			\
+				plat/arm/common/arm_nor_psci_mem_protect.c
+
+# Add the FDT_SOURCES and options for Dynamic Config
+FDT_SOURCES		+=	${RDKRONOS_BASE}/fdts/${PLAT}_fw_config.dts \
+				${RDKRONOS_BASE}/fdts/${PLAT}_tb_fw_config.dts
+FW_CONFIG		:=	${BUILD_PLAT}/fdts/${PLAT}_fw_config.dtb
+TB_FW_CONFIG		:=	${BUILD_PLAT}/fdts/${PLAT}_tb_fw_config.dtb
+
+# Add the FW_CONFIG to FIP and specify the same to certtool
+$(eval $(call TOOL_ADD_PAYLOAD,${FW_CONFIG},--fw-config,${FW_CONFIG}))
+# Add the TB_FW_CONFIG to FIP and specify the same to certtool
+$(eval $(call TOOL_ADD_PAYLOAD,${TB_FW_CONFIG},--tb-fw-config,${TB_FW_CONFIG}))
+
+include plat/arm/common/arm_common.mk
+include plat/arm/css/common/css_common.mk
+include plat/arm/board/common/board_common.mk
---
+++ plat/arm/board/rdkronos/rdkronos_plat.c
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2023, ARM Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <common/debug.h>
+#include <drivers/arm/sbsa.h>
+#include <lib/fconf/fconf.h>
+#include <lib/fconf/fconf_dyn_cfg_getter.h>
+#include <plat/arm/common/plat_arm.h>
+#include <plat/common/platform.h>
+
+const mmap_region_t plat_arm_mmap[] = {
+	ARM_MAP_SHARED_RAM,
+	KRONOS_MAP_DEVICE,
+	KRONOS_EXTERNAL_FLASH,
+	SOC_PLATFORM_PERIPH_MAP_DEVICE,
+	SOC_SYSTEM_PERIPH_MAP_DEVICE,
+#if IMAGE_BL2
+	KRONOS_MAP_NS_DRAM1,
+#endif
+	{0}
+};
+
+void plat_arm_secure_wdt_start(void)
+{
+	sbsa_wdog_start(SBSA_SECURE_WDOG_BASE, SBSA_SECURE_WDOG_TIMEOUT);
+}
+
+void plat_arm_secure_wdt_stop(void)
+{
+	sbsa_wdog_stop(SBSA_SECURE_WDOG_BASE);
+}
+
+/*
+ * For Kronos we do not do anything in these interface functions.
+ */
+void plat_arm_security_setup(void)
+{
+}
+
+void __init plat_arm_interconnect_init(void)
+{
+}
+
+void plat_arm_interconnect_enter_coherency(void)
+{
+}
+
+void plat_arm_interconnect_exit_coherency(void)
+{
+}
+
+void soc_css_init_nic400(void)
+{
+}
+
+void soc_css_init_pcie(void)
+{
+}
